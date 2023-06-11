541c0a38e34829a48565188905a3022c5cac74d6
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-06-11 16:29:50 +0900

    階段のような細かい当たり判定に対応。当たり判定の移動を考えたときの当たり判定で演算するように。

--- src/main/java/io/github/yuko1101/afktoinvincible/mixin/piston/PistonBlockMixin.java
+++ src/main/java/io/github/yuko1101/afktoinvincible/mixin/piston/PistonBlockMixin.java
@@ -13,20 +13,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(PistonBlock.class)
 public class PistonBlockMixin {
     @Inject(method = "isMovable", at = @At("HEAD"), cancellable = true)
     private static void isMovable(BlockState state, World world, BlockPos pos, Direction direction, boolean canBreak, Direction pistonDir, CallbackInfoReturnable<Boolean> cir) {
         // server side
         if (AfkToInvincibleServer.INSTANCE != null) {
-            final Box movedTo = state.getCollisionShape(world, pos.offset(direction)).getBoundingBox();
+            // TODO: それぞれのboxをピストンの向きと反対方向に移動させたboxとunionを使って統合させることで、ピストンの動きによる当たり判定の伸び(当たり判定の移動を考えたときの当たり判定)を無理やり処理している。もっと良い方法を考える。
+            final List<Box> movedTo = new ArrayList<>(state.getCollisionShape(world, pos).getBoundingBoxes().stream().map(box -> box.union(box.offset(new BlockPos(0, 0, 0).offset(direction.getOpposite())))).map(box -> box.offset(pos.offset(direction))).toList());
+            // TODO: ピストンの押し出し部の当たり判定が何故かなくなったりするため、当たり判定を無理やり作っていが、もっと良い方法を考える。
+            if (movedTo.isEmpty()) {
+                movedTo.add(new Box(pos));
+            }
+            AfkToInvincible.LOGGER.info("movedTo: " + movedTo);
 
-            // TODO: 動くブロックの上に放置している人がいる場合も、ピストンが動かないようにする
             final boolean isAfkPlayerIn = world.getPlayers().stream().anyMatch(player -> {
                 if (!AfkToInvincible.isAfk(player.getUuid(), false)) return false;
                 final Box boundingBox = player.getBoundingBox();
-                return boundingBox.intersects(movedTo);
+                AfkToInvincible.LOGGER.info("player: " + boundingBox);
+                return movedTo.stream().anyMatch(boundingBox::intersects);
             });
+
+            AfkToInvincible.LOGGER.info("isAfkPlayerIn: " + isAfkPlayerIn);
 
             if (isAfkPlayerIn) cir.setReturnValue(false);
         }
