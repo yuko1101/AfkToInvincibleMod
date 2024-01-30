1171b80b9477fef482d98ae76b094a3763bc484f
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-01-30 20:18:39 +0900

    fixed sweep attack won't be triggered when afk

--- src/main/java/io/github/yuko1101/afktoinvincible/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/yuko1101/afktoinvincible/mixin/PlayerEntityMixin.java
@@ -2,9 +2,11 @@ package io.github.yuko1101.afktoinvincible.mixin;
 
 import io.github.yuko1101.afktoinvincible.AfkToInvincible;
 import net.minecraft.entity.player.PlayerEntity;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -14,5 +16,10 @@ public abstract class PlayerEntityMixin extends LivingEntityMixin {
         if (AfkToInvincible.isAfk(getUuid(), getWorld().isClient)) {
             cir.setReturnValue(false);
         }
+    }
+
+    @Redirect(method = "attack", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;onGround:Z", opcode = Opcodes.GETFIELD))
+    private boolean attackOnGround(PlayerEntity playerEntity) {
+        return playerEntity.isOnGround() || AfkToInvincible.isAfk(getUuid(), getWorld().isClient);
     }
 }
